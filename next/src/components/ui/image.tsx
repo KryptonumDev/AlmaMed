6fb358f19cea89665bd37ed1ba72d35c239062a3
kryptonumstudio <kryptonumstudio@gmail.com>
2024-01-10 16:41:50 +0100

    Refactor Image component to update placeholder logic

--- next/src/components/ui/image.tsx
+++ next/src/components/ui/image.tsx
@@ -1,17 +1,14 @@
-
-import NextImage from "next/image"
+import NextImage from 'next/image';
 
 interface Props {
   data: any;
   className?: string;
 }
 
-const defaultPlaceholder = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8/v7NfwAJigPTkfv+7gAAAABJRU5ErkJggg==";
+const defaultPlaceholder =
+  'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8/v7NfwAJigPTkfv+7gAAAABJRU5ErkJggg==';
 
-export default function Image({
-  data,
-  ...props
-}: Props) {
+export default function Image({ data, ...props }: Props) {
   return (
     <NextImage
       src={data?.asset.url}
@@ -19,8 +16,12 @@ export default function Image({
       width={data?.asset.metadata?.dimensions?.width}
       height={data?.asset.metadata?.dimensions?.height}
       blurDataURL={data?.asset.metadata?.lqip || defaultPlaceholder}
-      placeholder="blur"
+      placeholder={
+        data?.asset.metadata?.dimensions?.width < 40 && data?.asset.metadata?.dimensions?.height < 40
+          ? undefined
+          : 'blur'
+      }
       {...props}
     />
-  )
+  );
 }
